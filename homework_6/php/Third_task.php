b50ab546adeb730629f74229088b9b6db650805c
kimarinov <58824713+kimarinov@users.noreply.github.com>
2020-01-05 23:37:37 +0200

    Initial commit

---
+++ homework_6/php/Third_task.php
@@ -0,0 +1,8 @@
+<?php
+echo date_default_timezone_set("Europe/Sofia");
+echo date_default_timezone_get();
+echo date(" H:i:sa");
+echo date_default_timezone_set("Australia/Sydney") . "</br>";
+echo date_default_timezone_get();
+echo date(" H:i:sa");
+?>
