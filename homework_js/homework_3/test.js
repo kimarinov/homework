130d70a9867ab88d25bc6932cd06d2ff66cfece8
kimarinov <58824713+kimarinov@users.noreply.github.com>
2020-06-04 09:33:50 +0300

    Update test.js
    s

--- homework_js/homework_3/test.js
+++ homework_js/homework_3/test.js
@@ -1,4 +1,4 @@
-var hex = 'aa'
+var hex = 'aaa'
 
-console.log((parseInt(hex, 16).toString(2)).padStart(8, '0'))
+console.log((parseInt(hex, 16).toString(3)).padStart(8, '0'))
 console.log((parseInt(hex, 16).toString(2)).padStart(8, '0'))
