0c95a79cb654b5df2e0898661b2a81450e4b08c9
kimarinov <58824713+kimarinov@users.noreply.github.com>
2020-07-24 16:07:15 +0300

    sunk

--- homework_js/projekt/script/script.js
+++ homework_js/projekt/script/script.js
@@ -2,6 +2,7 @@ let erroMsg = "Please fill corrct JSON input: {   'input': {'values': [value1, v
 
 $('#btn').on('click', function(event) {
     event.preventDefault();
+
     //delete text from <p>
     $('#error').text(null);
     $('#project p:nth-child(2)').text(null)
@@ -9,7 +10,6 @@ $('#btn').on('click', function(event) {
 
     let jsonArr = $('#textarea').val(),
         flag = true;
-
     //check for valid Json input
     try {
         let objArr = JSON.parse(jsonArr)
@@ -20,7 +20,7 @@ $('#btn').on('click', function(event) {
     let objArr = JSON.parse(jsonArr)
 
     //check for json array with input,values and output key
-    if (!objArr.input.values || !objArr.input || !objArr || !objArr.output) {
+    if (!objArr.input || !objArr.input.values || !objArr.output) {
         $('#error').text(erroMsg);
         flag = false;
     }
@@ -31,33 +31,41 @@ $('#btn').on('click', function(event) {
     let sum = 0,
         sum2 = 0,
         count = 0;
+
     while (count < len) {
+        //calculations only for positive elements
         if (arr[count] > 0) {
+            //sum from N-th elements to the end of the array
             for (var i = count; i < len; i++) {
                 sum += arr[i];
+                // if sum of array <= 0 the game is over, go in next cycle
                 if (sum <= 0) {
                     break;
                 }
             }
+            //check for game over
             if (sum > 0) {
-                for (var j = 0; j < count; j++) {
-                    sum2 += arr[j];
+                //sum from 0-th element to N-th element
+                for (var i = 0; i < count; i++) {
+                    sum2 += arr[i];
+                    //if sum of first cycle + sum from second cycle
+                    // <= 0, the game is over, go in next cycle
                     if ((sum + sum2) <= 0) {
                         break;
                     }
                 }
             }
-
+            //if sum of two cicles is above 0 push the number in arrOutput array 
             if ((sum + sum2) > 0) {
-
                 arrOutput.push(count);
             }
+            //reset sum for two cycles
             sum = 0;
             sum2 = 0;
         }
         count++;
     }
-
+    //if have valid input print arrOutput and output from JSON input
     if (flag = true) {
         $('#project p:nth-child(2)').text('lenth of output: ' + arrOutput.length + " the values is/are: ")
         $.each(arrOutput, function(index, val) {
